06a8857a7c65c52f1c08f98ea60692c90da82f68
cka001 <86254376+cka001@users.noreply.github.com>
2021-06-23 13:56:27 +0530

    Update postgres-connect.php

--- postgres-connect.php
+++ postgres-connect.php
@@ -1,43 +1,42 @@
+<h2>Attempting Postgres connection from php...</h2>
+
 <?php
 
+
 $host        = "host = ec2-54-228-174-49.eu-west-1.compute.amazonaws.com";
-$port        = "port = 5432";
-$dbname      = "dbname = dflortvvo307l5";
-$credentials = "user = hwmoqdvmjvljhr password=d13d6be7d2de8bde9552bdaa74a6ca5aad54fd48a3167e87ad9683d784e3add1";
-$db = pg_connect( "$host $port $dbname $credentials");
+   $port        = "port = 5432";
+   $dbname      = "dbname = dflortvvo307l5";
+   $credentials = "user = hwmoqdvmjvljhr password=d13d6be7d2de8bde9552bdaa74a6ca5aad54fd48a3167e87ad9683d784e3add1";
+   $db = pg_connect( "$host $port $dbname $credentials");
    
+if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Connected to database successfully\n";
+   }
 
-	 
-$name = $_GET['name'];
-$uname = $_GET['uname'];
-$email = $_GET['email'];
-$dob = $_GET['date'];
-$psw = $_GET['psw'];
-	
-echo $name<br>;
-echo $uname<br>;
-echo $email<br>;
-echo $dob<br>;
-echo $psw<br>;
+echo "<br>";
 
-$query =<<<EOF
-	INSERT INTO CLIENT_DETAILS(C_NAME, C_UNAME, C_EMAIL, C_DOB, C_PASS) 
-	VALUES ('$name', '$uname', '$email', '$dob', '$psw')";
-	
+$name = $_POST['name'];
+	$dob = $_POST['date'];
+	$email = $_POST['email'];
+	$psw = $_POST['psw'];
+	$mob = $_POST['mob'];
+
+echo"<h4>Sending these detailes to the database:</h4>Name: $name<br> DOB: $dob<br> Email: $email<br> Passsword: $psw<br> Mobile: $mob<br>";
+
+echo "<br><br>";
+
+$sql =<<<EOF
+      INSERT INTO STUDENT_DETAILS (S_NAME, S_DOB, S_EMAIL, S_PASS, S_MOB)
+      VALUES ('$name', '$dob', '$email', '$psw', '$mob');
 EOF;
-	
-$ret = pg_query($db, $query);
-	 
- if(!$ret)
-{
-	echo pg_last_error($db);
-   }
-else{
-   echo "Record successfully added to the database\n";
-   }
 
-pg_close($db);
-	    	
-		 
-
+   $ret = pg_query($db, $sql);
+   if(!$ret) {
+      echo pg_last_error($db);
+   } else {
+      echo "Record successfully added to database\n";
+   }
+   pg_close($db);
 ?>
